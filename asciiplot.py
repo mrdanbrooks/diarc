0a190d62526b8d136806cd3745d06a86534b4d03
dan brooks <dan@danbrooks.net>
2014-06-10 18:18:28 -0700

    [db] cleans up unused code from asciiplot

--- asciiplot.py
+++ asciiplot.py
@@ -5,39 +5,6 @@ from parser import *
 
 vertexSpacing = 5
 
-class GVertex(object):
-    def __init__(self,vertex,leftIdx):
-        typecheck(vertex,Vertex,"vertex")
-        typecheck(leftIdx,int,"leftIdx")
-        self._vertex = vertex
-        self.leftIdx = leftIdx # Leftmost index of vertex box
-
-        # Make a list of available columns
-        self.collectorCols = dict() # altitude: column
-        collectors = self._vertex.collectors()
-        for i,c in zip(range(len(collectors)),collectors):
-            self.collectorCols[c.altitude] = 2+(i*2)
-        self.emitterCols = dict() # altitude: column
-        emitters = self._vertex.emitters()
-        for i,e in zip(range(len(emitters)),emitters):
-            self.emitterCols[e.altitude] = 2+len(self.collectorCols)*2+(i*2)
-
-class GEdge(object):
-    def __init__(self,srcCol,sinkCol,row):
-        self.srcCol = srcCol
-        self.sinkCol = sinkCol
-        self.row = row
-
-
-
-# def calcVertexWidth(vertex):
-#     """ Calculate the total width of a vertex. Base width is 5
-#     +-+-+ with additional 2 spaces per connection.
-#     | | |
-#     +-+-+
-#     """
-#     return 5+2*(len(vertex.emitters)+len(vertex.collectors))
-
 class Plot(object):
     def __init__(self,topology):
         self.topology = topology
@@ -220,97 +187,9 @@ def draw(topology):
         else:
             raise Exception("invalid altitude")
 
-
-
-            
-
     print grid
         
     
-
-
-
-
-    exit(0)
-    vboxes = dict() # TypedList(GVertex)
-    gedges = list() # TypedList(GEdge)
-    
-    # Draw the vertices boxes
-    cidx = 0 # the leftmost column in which to draw the box
-    for v in topology.vertices:
-        vbox = GVertex(v,cidx)
-        vboxes[v.index] = vbox
-        # Figure out how many connections are going inside this box
-        conns = len(v.emitters())+len(v.collectors())
-        grid.writeStr((0,cidx),"+-"+"--"*conns+"+")
-        grid.writeStr((1,cidx),"| "+"  "*conns+"|")
-        grid.writeStr((2,cidx),"+-"+"--"*conns+"+")
-        # Move leftmost drawing index to start for next box
-        cidx += 2+(2*conns)
-        cidx += 5
-
-    # Calculate the new size of the grid
-    altitudes = [e.altitude for e in topology.edges]
-    maxAltitude = max(altitudes)
-    minAltitude = min(altitudes)
-    grid.insertRowsAbove(0,maxAltitude+1)
-
-    # Get the row that runs the center of the vertex boxes
-    vridx = 1+maxAltitude+1 
-
-    # Calculate edge connections
-    for edge in topology.edges:
-        altitude = edge.altitude
-        row = vridx + (-altitude-2 if altitude > 0 else abs(altitude)+2)
-        for srcVertex in edge.sources():
-            src = vboxes[srcVertex.index]
-            srcCol = src.emitterCols[altitude]+src.leftIdx
-            for sinkVertex in edge.sinks():
-                sink = vboxes[sinkVertex.index]
-                sinkCol = sink.collectorCols[altitude]+sink.leftIdx
-                gedges.append(GEdge(srcCol,sinkCol,row)) 
-    # Draw edges
-    for gedge in gedges:
-        if gedge.row < vridx:
-           grid[(vridx-1,gedge.srcCol)] = 'A' 
-           grid[(vridx-1,gedge.sinkCol)] = 'V'
-           dist = gedge.sinkCol - gedge.srcCol - 1
-           # Draw altitude lines. The end of each line wraps down with a '.' and
-           # the middle has '-' filling. Do not let the fill overwrite the end
-           # markers
-           grid[(gedge.row,gedge.srcCol+1)] = "."
-           for i in range(dist-1):
-               if not (gedge.row,gedge.srcCol+2+i) in grid:
-                   grid[(gedge.row,gedge.srcCol+2+i)] = '-'
-           grid[(gedge.row,gedge.srcCol+(dist-1)+1)] = "."
-
-           # Draw the vertical connections
-           for r in range(gedge.row+1,vridx-1):
-               grid[(r,gedge.srcCol)] = '|'
-               grid[(r,gedge.sinkCol)] = '|'
-
-        if gedge.row > vridx:
-           grid[(vridx+1,gedge.srcCol)] = 'V' 
-           grid[(vridx+1,gedge.sinkCol)] = 'A' 
-
-           dist = gedge.srcCol - gedge.sinkCol - 1
-           # Draw altitude lines. The end of each line wraps down with a '.' and
-           # the middle has '-' filling. Do not let the fill overwrite the end
-           # markers
-           grid[(gedge.row,gedge.sinkCol+1)] = "'"
-           for i in range(dist-1):
-               if not (gedge.row,gedge.sinkCol+2+i) in grid:
-                   grid[(gedge.row,gedge.sinkCol+2+i)] = '-'
-           grid[(gedge.row,gedge.sinkCol+(dist-1)+1)] = "'"
-           
-           # Draw the vertical connections
-           for r in range(vridx+2,gedge.row):
-               grid[(r,gedge.srcCol)] = '|'
-               grid[(r,gedge.sinkCol)] = '|'
-
-    print grid
-
-
 if __name__ == "__main__":
     topology = parseFile("v3.xml")
     draw(topology)
