7d6a83730b93ff55cf42e164df80e2a903f5affa
dan brooks <dan@danbrooks.net>
2014-06-16 13:43:08 -0700

    [db] updates asciiplot to draw new topology objects - working!

--- asciiplot.py
+++ asciiplot.py
@@ -8,46 +8,45 @@ vertexSpacing = 5
 class Plot(object):
     def __init__(self,topology):
         self.topology = topology
-        # Width of the entire plot
-        self.height =  len(self.topology.edges)+5
         # height of the entire plot
-        self.width =  vertexSpacing*(len(self.topology.vertices)-1) + sum([self.vertexWidth(v) for v in self.topology.vertices])
+        self.height =  len(self.topology.bands)+5
+        # Width of the entire plot
+        self.width =  vertexSpacing*(len(self.topology.blocks)-1) + sum([self.blockWidth(v) for v in self.topology.blocks])
         # Row of the center of the vertex boxes
-        self.vline = len(filter(lambda x: x.altitude >0,self.topology.edges.values())) + 2
+        self.vline = len(filter(lambda x: x.altitude >0,self.topology.bands.values())) + 2
 
-    def vertexWidth(self,index):
-        """ Calculate the total width of a vertex. Base width is 5
+    def blockWidth(self,index):
+        """ Calculate the total width of a vertex block. Base width is 5
         +-+-+ with additional 2 spaces per connection.
         | | |
         +-+-+
         """
-        vertex = self.topology.vertices[index]
-        return 5+2*(len(vertex.emitters)+len(vertex.collectors))
+        vertexBlock = self.topology.blocks[index]
+        return 5+2*(len(vertexBlock.emitter)+len(vertexBlock.collector))
 
-    def vertexLeftCol(self,index):
-        """ Calculates the left column index for this vertex index """
-        vertex = self.topology.vertices[index]
-        return sum([self.vertexWidth(i) for i in range(index)]) + vertexSpacing*index
+    def blockLeftCol(self,index):
+        """ Calculates the left column index for this vertex block index """
+        return sum([self.blockWidth(i) for i in range(index)]) + vertexSpacing*index
 
-    def vertexCenterCol(self,index):
+    def blockCenterCol(self,index):
         """ Calculates the center column index for this vertex index """
-        vertex = self.topology.vertices[index]
-        return self.vertexLeftCol(index)+4+(2*max(vertex.collectors))
+        vertexBlock = self.topology.blocks[index]
+        return self.blockLeftCol(index)+2+(2*len(vertexBlock.collector))
 
-    def vertexRightCol(self,index):
+    def blockRightCol(self,index):
         """ Calculates the right column index for this vertex index """
-        vertex = self.topology.vertices[index]
-        return self.vertexCenterCol(index)+4+(2*max(vertex.emitters))
+        vertexBlock = self.topology.blocks[index]
+        return self.blockCenterCol(index)+2+(2*len(vertexBlock.emitter))
 
-    def vertexCollectorCol(self,vIndex,order):
+    def blockCollectorCol(self,vIndex,order):
         """ Calculates the column index for a collector at 'order' in vertex vIndex """
-        return self.vertexLeftCol(vIndex)+2+(order*2)
+        return self.blockLeftCol(vIndex)+2+(order*2)
 
-    def vertexEmitterCol(self,vIndex,order):
+    def blockEmitterCol(self,vIndex,order):
         """ Calculates the column index for an emitter at 'order' in vertex vIndex """
-        return self.vertexCenterCol(vIndex)+2+(order*2)
+        return self.blockCenterCol(vIndex)+2+(order*2)
 
-    def edgeRow(self,altitude):
+    def bandRow(self,altitude):
         """ Returns the row index for this altitude """
         magnitude = 2 + abs(altitude)
         return self.vline-magnitude if altitude > 0 else self.vline+magnitude
@@ -59,92 +58,72 @@ def draw(topology):
     # Initialize Grid to use as a canvas
     grid = CharGrid()
 
-    # Calculate the number of rows needed for the drawing:
-    # 3 rows for vertices
-    # 2 rows as space (for above above and below vertices)
-    # 1 row per edge
+    # Generate plot calculator
     p = Plot(topology)
 
-
-    # Draw the vertex boxes - count number of positive altitude lines, add 2
-    vline = p.vline
-
-    
+    # Draw the vertex boxes
     # TODO: Check that there are no negative value keys
-
-    for k in topology.vertices.keys():
-        vertex = topology.vertices[k]
-        leftCol = p.vertexLeftCol(k)
+    vline = p.vline
+    for k in topology.blocks:
+        vertexBlock = topology.blocks[k]
+        leftCol = p.blockLeftCol(k)
 
         # Draw the left side of the box
         grid[(p.vline-1,leftCol)] = '+-'
         grid[(p.vline,leftCol)] = '| '
         grid[(p.vline+1,leftCol)] = '+-'
 
-        # Draw Collector connections
-        for o in vertex.collectors:
-            for edge in vertex.collectors[o]:
-                if edge is None:
-                    continue
-                col = p.vertexCollectorCol(k,o)
-                if edge.altitude > 0:
-                    grid[(p.vline-1,col)] = 'V-'
-                    grid[(p.vline+1,col)] = '--'
-                elif edge.altitude < 0:
-                    grid[(p.vline+1,col)] = 'A-'
-                    grid[(p.vline-1,col)] = '--'
-
+        # Draw Collector spacers
+        for o in vertexBlock.collector:
+            col = p.blockCollectorCol(k,o)
+            grid[(p.vline+1,col)] = '--'
+            grid[(p.vline-1,col)] = '--'
+ 
         # Draw the middle line
-        centerCol = p.vertexCenterCol(k)
+        centerCol = p.blockCenterCol(k)
         grid[(p.vline-1,centerCol)] = '+-'
         grid[(p.vline,centerCol)] = '| '
         grid[(p.vline+1,centerCol)] = '+-'
 
-        # Draw the emitter connections
-        for o in vertex.emitters:
-            for edge in vertex.emitters[o]:
-                if edge is None:
-                    continue
-                col = p.vertexEmitterCol(k,o)
-                if edge.altitude > 0:
-                    grid[(p.vline-1,col)] = 'A-'
-                    if not (p.vline+1,col) in grid:
-                        grid[(p.vline+1,col)] = '--'
-                elif edge.altitude < 0:
-                    grid[(p.vline+1,col)] = 'V-'
-                    if not (p.vline-1,col) in grid:
-                        grid[(p.vline-1,col)] = '--'
-
+        # Draw the emitter spacers
+        for o in vertexBlock.emitter:
+            col = p.blockEmitterCol(k,o)
+            grid[(p.vline+1,col)] = '--'
+            grid[(p.vline-1,col)] = '--'
+ 
         # Draw the right line
-        rightCol = p.vertexRightCol(k)
+        rightCol = p.blockRightCol(k)
         grid[(p.vline-1,rightCol)] = '+'
         grid[(p.vline,rightCol)] = '|'
         grid[(p.vline+1,rightCol)] = '+'
 
 
     # Draw Edge Lines
-    # Go through each line segment (a source to a sink) and draw the line
-    for key in topology.edges:
-        edge = topology.edges[key]
-        altitude = edge.altitude
-        row = p.edgeRow(altitude)
-#         grid[(row,0)] = str(altitude)
-        if altitude > 0:
-            # Drawing Routings for lines above the vertex boxes
-            for src in edge.sources:
-                srcOrder = edge.sources[src].emitters.reverseLookup(edge)
-                srcCol = p.vertexEmitterCol(src,srcOrder)
+    for edge in topology.edges:
+        for source in edge.sources:
+            srcSnap = source.snap
+            srcOrder = source.snap.order
+            srcCol = p.blockEmitterCol(srcSnap.block.index,srcOrder)
+            for sink in edge.sinks:
+                sinkSnap = sink.snap
+                sinkOrder = sinkSnap.order
+                sinkCol = p.blockCollectorCol(sinkSnap.block.index,sinkOrder)
 
-                # Draw the vertical source line
-                grid[(row,srcCol+1)] = '.'
-                for i in range(vline-2-row):
-                    grid[(vline-2-i,srcCol)] = '|'
+                # Determine if connection should be on top or bottom
+                # Positive band connection
+                if source.block.index < sink.block.index and edge._pBand is not None:
+                    altitude = edge.posBand.altitude
+                    row = p.bandRow(altitude)
+                    grid[(row,0)] = str(altitude)
 
-                for sink in edge.sinks:
-                    sinkOrder = edge.sinks[sink].collectors.reverseLookup(edge)
-                    sinkCol = p.vertexCollectorCol(sink,sinkOrder)
+                    # Draw the vertical source line
+                    grid[(p.vline-1,srcCol)] = 'A-'
+                    grid[(row,srcCol+1)] = '.'
+                    for i in range(vline-2-row):
+                        grid[(vline-2-i,srcCol)] = '|'
 
                     # Draw the vertical sink line
+                    grid[(p.vline-1,sinkCol)] = 'V-'
                     grid[(row,sinkCol-1)] = '.'
                     for i in range(vline-2-row):
                         grid[(vline-2-i,sinkCol)] = '|'
@@ -154,22 +133,20 @@ def draw(topology):
                         if not (row,srcCol+2+x) in grid:
                             grid[(row,srcCol+2+x)] = '-'
 
-        elif altitude < 0:
-            # Drawing routines for linse below the vertex boxes
-            for src in edge.sources:
-                srcOrder = edge.sources[src].emitters.reverseLookup(edge)
-                srcCol = p.vertexEmitterCol(src,srcOrder)
+                # Negative band connection
+                elif source.block.index > sink.block.index and edge._nBand is not None: 
+                    altitude = edge.negBand.altitude
+                    row = p.bandRow(altitude)
+                    grid[(row,0)] = str(altitude)
 
-                # Draw the vertical source line
-                grid[(row,srcCol-1)] = "'"
-                for i in range(row-(vline+2)):
-                    grid[(vline+2+i,srcCol)] = '|'
-
-                for sink in edge.sinks:
-                    sinkOrder = edge.sinks[sink].collectors.reverseLookup(edge)
-                    sinkCol = p.vertexCollectorCol(sink,sinkOrder)
+                    # Draw the vertical source line
+                    grid[(p.vline+1,srcCol)] = 'V-'
+                    grid[(row,srcCol-1)] = "'"
+                    for i in range(row-(vline+2)):
+                        grid[(vline+2+i,srcCol)] = '|'
 
                     # Draw the vertical sink line
+                    grid[(p.vline+1,sinkCol)] = 'A-'
                     grid[(row,sinkCol+1)] = "'"
                     for i in range(row-(vline+2)):
                         grid[(vline+2+i,sinkCol)] = '|'
@@ -178,16 +155,13 @@ def draw(topology):
                     for x in range(srcCol-sinkCol-2):
                         if not (row,sinkCol+2+x) in grid:
                             grid[(row,sinkCol+2+x)] = '-'
-
-
-        else:
-            raise Exception("invalid altitude")
+                else:
+                    continue
 
     print grid
  
     
 if __name__ == "__main__":
-    topology = parseFile("data/v3.xml")
+    topology = parseFile("data/v4.xml")
     print ""
     draw(topology)
-
