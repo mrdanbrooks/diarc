665b2dfb8136b91b01c7f6fc0f9fbe825ced57f4
dan brooks <dan@danbrooks.net>
2014-06-17 16:26:00 -0700

    [db] adds a test file

---
+++ test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# Usage:
+# ./test.py topology_plot data/v5.xml
+
+import sys
+import inspect
+
+def ros_plot():
+    """ Test drawing a ROS xml specification """
+    import ros_parser
+    import asciiplot
+    ros = ros_parser.parseFile("data/dava.xml")
+    asciiplot.draw(ros)
+
+
+def topology_plot(args):
+    """ draw an xml topology in ascii art """
+    import parser
+    import asciiplot
+    print "Opening file",args[0]
+
+    topology = parser.parseFile(args[0])
+    print ""
+    asciiplot.draw(topology)
+
+
+if __name__=="__main__":
+    available_tests = dict(inspect.getmembers(sys.modules[__name__],inspect.isfunction))
+    if sys.argv[1] not in available_tests:
+        print "Test available:",available_tests.keys()
+        exit(0)
+    elif len(sys.argv)>2:
+        available_tests[sys.argv[1]](sys.argv[2:])
+    else:
+        available_tests[sys.argv[1]]()
+
