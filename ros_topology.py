9862d900dbc774fbf5000932f6267b9d386cd2ef
dan brooks <dan@danbrooks.net>
2014-06-17 12:05:37 -0700

    [db] adds dumbplacement technique for ros_topology as placeholder

--- ros_topology.py
+++ ros_topology.py
@@ -43,8 +43,8 @@ class Topic(Edge):
         super(Topic,self).__init__(rsg)
         
         # Dumb placement - just get the enxt free altitudes
-        
         self.posBand.altitude,self.negBand.altitude = rsg.nextFreeAltitudes()
+
         self.name = None
         self.msgType = None
         self.bandwidth = None
@@ -59,6 +59,10 @@ class Publisher(Source):
         typecheck(node,Node,"node")
         typecheck(topic,Topic,"topic")
         super(Publisher,self).__init__(rsg,node,topic)
+
+        # Dumb placement
+        self.snap.order = max([pub.snap.order for pub in node.publishers] + [0])
+
         self.bandwidth = None
         self.msgType = None
 
@@ -68,6 +72,10 @@ class Subscriber(Sink):
         typecheck(node,Node,"node")
         typecheck(topic,Topic,"topic")
         super(Subscriber,self).__init__(rsg,node,topic)
+
+        # Dumb placement
+        self.snap.order = max([sub.snap.order for sub in node.subscribers] + [0])
+
         self.bandwidth = None
         self.msgType = None
 
