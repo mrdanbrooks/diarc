04405c5938f1f9591d89157457c1038d0888f43d
dan brooks <dan@danbrooks.net>
2014-06-19 11:32:24 -0700

    [db] reorganized unit tests and added tests for checking that snaps only report bands they will really connect to

--- tests.py
+++ tests.py
@@ -1,16 +1,15 @@
 import unittest
+import types
 
 
-class TestBandEmittersCollectors(unittest.TestCase):
-    """ A Band object's emitters and collectors should only consist of the 
-    subset of snaps which will actually be drawn touching the band. Some snaps
-    have links to a band they will not touch because the snap corresponds to a
-    connection with an edge the follows the alternative band. 
-    """
-    def test_v5_a(self):
+
+class Test_v5_a(unittest.TestCase):
+    def setUp(self):
         import parser
-        # TestFile v5_a.xml
-        t = parser.parseFile('data/v5_a.xml')
+        self.t = parser.parseFile('data/v5_a.xml')
+ 
+    def test_band_emitters_collectors(self):
+        t = self.t
         # There should be exactly 1 edge with two bands
         assert(len(t.edges) == 1)
         assert(len(t.edges[0].bands) == 2)
@@ -30,10 +29,38 @@ class TestBandEmittersCollectors(unittest.TestCase):
         assert(t.edges[0].negBand.collectors[0].block.index == 1)
         assert(t.edges[0].posBand.collectors[0] == t.edges[0].negBand.collectors[0])
 
-    def test_v5_b(self):
+    def test_block(self):
+        t = self.t
+        # Make sure the matching is correct
+        for index in t.blocks:
+            assert(t.blocks[index].index == index)
+
+        # Count the number of emitters and collectors of each block
+        # {blockIndex: (#collectors,#emitters), ...}
+        vals = { 0: (0,1),  1: (1,0),  2: (0,1)}
+        for index in t.blocks:
+            assert(len(t.blocks[index].collector) == vals[index][0])
+            assert(len(t.blocks[index].emitter) == vals[index][1])
+
+    def test_snaps_to_bands_connectivity(self):
+        """ Test to make sure that snaps are connected to the correct bands """
+        t = self.t
+        assert(t.blocks[0].emitter[0].posBand.altitude == 1)
+        assert(t.blocks[0].emitter[0].negBand is None)
+        assert(t.blocks[1].collector[0].posBand.altitude == 1)
+        assert(t.blocks[1].collector[0].negBand.altitude == -1)
+        assert(t.blocks[2].emitter[0].posBand is None)
+        assert(t.blocks[2].emitter[0].negBand.altitude == -1)
+
+
+
+class Test_v5_b(unittest.TestCase):
+    def setUp(self):
         import parser
-        t = parser.parseFile('data/v5_b.xml')
-        
+        self.t = parser.parseFile('data/v5_b.xml')
+ 
+    def test_band_emitters_collectors(self):
+        t = self.t
         # There should be exactly 1 edge with two bands
         assert(len(t.edges) == 1)
         assert(len(t.edges[0].bands) == 2)
@@ -53,13 +80,41 @@ class TestBandEmittersCollectors(unittest.TestCase):
         assert(t.edges[0].negBand.emitters[0].block.index == 1)
         assert(t.edges[0].posBand.emitters[0] == t.edges[0].negBand.emitters[0])
 
-    def test_v5_c(self):
+    def test_block(self):
+        t = self.t
+        # Make sure the matching is correct
+        for index in t.blocks:
+            assert(t.blocks[index].index == index)
+
+        # Count the number of emitters and collectors of each block
+        # {blockIndex: (#collectors,#emitters), ...}
+        vals = { 0: (1,0),  1: (0,1),  2: (1,0)}
+        for index in t.blocks:
+            assert(len(t.blocks[index].collector) == vals[index][0])
+            assert(len(t.blocks[index].emitter) == vals[index][1])
+
+    def test_snaps_to_bands_connectivity(self):
+        """ Test to make sure that snaps are connected to the correct bands """
+        t = self.t
+        assert(t.blocks[0].collector[0].posBand is None)
+        assert(t.blocks[0].collector[0].negBand.altitude == -1)
+        assert(t.blocks[1].emitter[0].posBand.altitude == 1)
+        assert(t.blocks[1].emitter[0].negBand.altitude == -1)
+        assert(t.blocks[2].collector[0].posBand.altitude == 1)
+        assert(t.blocks[2].collector[0].negBand is None)
+
+
+
+class Test_v5_c(unittest.TestCase):
+    def setUp(self):
         import parser
-        t = parser.parseFile('data/v5_c.xml')
-        
+        self.t = parser.parseFile('data/v5_c.xml')
+ 
+    def test_band_emitters_collectors(self):
+        t = self.t
         # There should be exactly 1 edge with two bands
         assert(len(t.edges) == 1)
-        assert(len(t.edges[0].bands) == 2)
+        assert(len(t.edges[0].bands) == 1)
 
         # The top band should have one emitter and two collectors
         assert(len(t.edges[0].posBand.emitters) == 1)
@@ -68,13 +123,39 @@ class TestBandEmittersCollectors(unittest.TestCase):
         assert(len(t.edges[0].negBand.emitters) == 0)
         assert(len(t.edges[0].negBand.collectors) == 0)
 
-    def test_v5_d(self):
+    def test_block(self):
+        t = self.t
+        # Make sure the matching is correct
+        for index in t.blocks:
+            assert(t.blocks[index].index == index)
+
+        # Count the number of emitters and collectors of each block
+        # {blockIndex: (#collectors,#emitters), ...}
+        vals = { 0: (0,1),  1: (1,0),  2: (1,0)}
+        for index in t.blocks:
+            assert(len(t.blocks[index].collector) == vals[index][0])
+            assert(len(t.blocks[index].emitter) == vals[index][1])
+
+    def test_snaps_to_bands_connectivity(self):
+        """ Test to make sure that snaps are connected to the correct bands """
+        t = self.t
+        assert(t.blocks[0].emitter[0].posBand.altitude == 1)
+        assert(t.blocks[0].emitter[0].negBand is None)
+        assert(t.blocks[1].collector[0].posBand.altitude == 1)
+        assert(t.blocks[1].collector[0].negBand is None)
+        assert(t.blocks[2].collector[0].posBand.altitude == 1)
+        assert(t.blocks[2].collector[0].negBand is None)
+
+class Test_v5_d(unittest.TestCase):
+    def setUp(self):
         import parser
-        t = parser.parseFile('data/v5_d.xml')
-        
+        self.t = parser.parseFile('data/v5_d.xml')
+ 
+    def test_band_emitters_collectors(self):
+        t = self.t
         # There should be exactly 1 edge with two bands
         assert(len(t.edges) == 1)
-        assert(len(t.edges[0].bands) == 2)
+        assert(len(t.edges[0].bands) == 1)
 
         # The top band should have nothing
         assert(len(t.edges[0].posBand.emitters) == 0)
@@ -83,21 +164,42 @@ class TestBandEmittersCollectors(unittest.TestCase):
         assert(len(t.edges[0].negBand.emitters) == 2)
         assert(len(t.edges[0].negBand.collectors) == 1)
 
+    def test_block_index(self):
+        """ checks that topology block indexing follows block index values """
+        t = self.t
+        # Make sure the matching is correct
+        for index in t.blocks:
+            assert(t.blocks[index].index == index)
 
-    def test_v5_e(self):
+    def test_emitter_collector_count(self):
+        """ Counts the number of emitters and collectors in each block """
+        # {blockIndex: (#collectors,#emitters), ...}
+        vals = { 0: (1,0),  1: (0,1),  2: (0,1)}
+        t = self.t
+        for index in t.blocks:
+            assert(len(t.blocks[index].collector) == vals[index][0])
+            assert(len(t.blocks[index].emitter) == vals[index][1])
+
+    def test_snaps_connectivity(self):
+        """ Test to make sure that snaps are connected to the correct bands """
+        t = self.t
+        assert(t.blocks[0].collector[0].posBand is None)
+        assert(t.blocks[0].collector[0].negBand.altitude == -1)
+        assert(t.blocks[1].emitter[0].posBand is None)
+        assert(t.blocks[1].emitter[0].negBand.altitude == -1)
+        assert(t.blocks[2].emitter[0].posBand is None)
+        assert(t.blocks[2].emitter[0].negBand.altitude == -1)
+
+class Test_v5_e(unittest.TestCase):
+    def setUp(self):
         import parser
-        t = parser.parseFile('data/v5_e.xml')
-        
+        self.t = parser.parseFile('data/v5_e.xml')
+ 
+    def test_band_emitters_collectors(self):
+        t = self.t
         # There should be exactly 1 edge with two bands
         assert(len(t.edges) == 1)
         assert(len(t.edges[0].bands) == 2)
-
-        # The top band should have two sources and two sinks
-        assert(len(t.edges[0].posBand.emitters) == 2)
-        assert(len(t.edges[0].posBand.collectors) == 2)
-        # The bottom band should have one source and one sink
-        assert(len(t.edges[0].negBand.emitters) == 1)
-        assert(len(t.edges[0].negBand.collectors) == 1)
 
         # Check locations
         assert([snap.block.index for snap in t.edges[0].posBand.emitters] == [0,2])
@@ -105,26 +207,79 @@ class TestBandEmittersCollectors(unittest.TestCase):
         assert([snap.block.index for snap in t.edges[0].negBand.emitters] == [2])
         assert([snap.block.index for snap in t.edges[0].negBand.collectors] == [1])
 
-    def test_v5_f(self):
+    def test_block_index(self):
+        """ checks that topology block indexing follows block index values """
+        t = self.t
+        # Make sure the matching is correct
+        for index in t.blocks:
+            assert(t.blocks[index].index == index)
+
+    def test_emitter_collector_count(self):
+        """ Counts the number of emitters and collectors in each block """
+        # {blockIndex: (#collectors,#emitters), ...}
+        vals = { 0: (0,1),  1: (1,0),  2: (0,1), 3: (1,0)}
+        t = self.t
+        for index in t.blocks:
+            assert(len(t.blocks[index].collector) == vals[index][0])
+            assert(len(t.blocks[index].emitter) == vals[index][1])
+
+    def test_snaps_connectivity(self):
+        """ Test to make sure that snaps are connected to the correct bands """
+        t = self.t
+        assert(t.blocks[0].emitter[0].posBand.altitude == 1)
+        assert(t.blocks[0].emitter[0].negBand is None)
+        assert(t.blocks[1].collector[0].posBand.altitude == 1)
+        assert(t.blocks[1].collector[0].negBand.altitude == -1)
+        assert(t.blocks[2].emitter[0].posBand.altitude == 1)
+        assert(t.blocks[2].emitter[0].negBand.altitude == -1)
+        assert(t.blocks[3].collector[0].posBand.altitude == 1)
+        assert(t.blocks[3].collector[0].negBand is None)
+
+
+class Test_v5_f(unittest.TestCase):
+    def setUp(self):
         import parser
-        t = parser.parseFile('data/v5_f.xml')
-        
-        # There should be exactly 1 edge with two bands
-        assert(len(t.edges) == 1)
-        assert(len(t.edges[0].bands) == 2)
-
-        # The bottom band should have two sources and two sinks
-        assert(len(t.edges[0].negBand.emitters) == 2)
-        assert(len(t.edges[0].negBand.collectors) == 2)
-        # The top band should have one source and one sink
-        assert(len(t.edges[0].posBand.emitters) == 1)
-        assert(len(t.edges[0].posBand.collectors) == 1)
-
+        self.t = parser.parseFile('data/v5_f.xml')
+ 
+    def test_band_emitters_collectors(self):
+        t = self.t
+       
         # Check locations
         assert([snap.block.index for snap in t.edges[0].posBand.emitters] == [1])
         assert([snap.block.index for snap in t.edges[0].posBand.collectors] == [2])
         assert([snap.block.index for snap in t.edges[0].negBand.emitters] == [1,3])
         assert([snap.block.index for snap in t.edges[0].negBand.collectors] == [0,2])
+
+    def test_block_index(self):
+        """ checks that topology block indexing follows block index values """
+        t = self.t
+        # Make sure the matching is correct
+        for index in t.blocks:
+            assert(t.blocks[index].index == index)
+
+    def test_emitter_collector_count(self):
+        """ Counts the number of emitters and collectors in each block """
+        # {blockIndex: (#collectors,#emitters), ...}
+        vals = { 0: (1,0),  1: (0,1),  2: (1,0), 3: (0,1)}
+        t = self.t
+        for index in t.blocks:
+            assert(len(t.blocks[index].collector) == vals[index][0])
+            assert(len(t.blocks[index].emitter) == vals[index][1])
+
+    def test_snaps_connectivity(self):
+        """ Test to make sure that snaps are connected to the correct bands """
+        t = self.t
+        assert(t.blocks[0].collector[0].negBand.altitude == -1)
+        assert(t.blocks[0].collector[0].posBand is None)
+        assert(t.blocks[1].emitter[0].negBand.altitude == -1)
+        assert(t.blocks[1].emitter[0].posBand.altitude == 1)
+        assert(t.blocks[2].collector[0].negBand.altitude == -1)
+        assert(t.blocks[2].collector[0].posBand.altitude == 1)
+        assert(t.blocks[3].emitter[0].negBand.altitude == -1)
+        assert(t.blocks[3].emitter[0].posBand is None)
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
