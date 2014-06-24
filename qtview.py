3ddcae4d8efa23b3514c55b5bf4f62c797f618d3
dan brooks <dan@danbrooks.net>
2014-06-23 18:02:49 -0700

    [db] adds emitter and collector objects

--- qtview.py
+++ qtview.py
@@ -7,6 +7,7 @@ import sys
 
 
 class MyBlock(QGraphicsWidget):
+    """ Visual Block, with right hand spacer """
     def __init__(self,parent,block):
         super(MyBlock,self).__init__(parent=parent)
         self.parent = parent
@@ -14,6 +15,29 @@ class MyBlock(QGraphicsWidget):
         self.block.visual = self
         self.setContentsMargins(5,5,5,5)
         self.rightSpacer = MyBlockSpacer(self)
+
+        l = self.parent.layout()
+        # Set up top and bottom margin
+        self._topMargin = MyBlockHorizontalSpacer(self)
+        self._botMargin = MyBlockHorizontalSpacer(self)
+        l.addAnchor(self,Qt.AnchorTop, self._topMargin, Qt.AnchorTop)
+        l.addAnchor(self,Qt.AnchorLeft, self._topMargin, Qt.AnchorLeft)
+        l.addAnchor(self,Qt.AnchorRight, self._topMargin, Qt.AnchorRight)
+        l.addAnchor(self,Qt.AnchorBottom, self._botMargin, Qt.AnchorBottom)
+        l.addAnchor(self,Qt.AnchorLeft, self._botMargin, Qt.AnchorLeft)
+        l.addAnchor(self,Qt.AnchorRight, self._botMargin, Qt.AnchorRight)
+
+
+        # Set up Emitter and Collector Containers
+        self.myEmitter = MyContainer(self)
+        self.myCollector = MyContainer(self)
+        l.addAnchor(self._topMargin, Qt.AnchorBottom, self.myEmitter, Qt.AnchorTop)
+        l.addAnchor(self._botMargin, Qt.AnchorTop, self.myEmitter, Qt.AnchorBottom)
+        l.addAnchor(self._topMargin, Qt.AnchorBottom, self.myCollector, Qt.AnchorTop)
+        l.addAnchor(self._botMargin, Qt.AnchorTop, self.myCollector, Qt.AnchorBottom)
+        l.addAnchor(self, Qt.AnchorLeft, self.myCollector, Qt.AnchorLeft)
+        l.addAnchor(self, Qt.AnchorRight, self.myEmitter, Qt.AnchorRight)
+        l.addAnchor(self.myCollector, Qt.AnchorRight, self.myEmitter, Qt.AnchorLeft)
 
     def link(self):
         l = self.parent.layout()
@@ -39,25 +63,62 @@ class MyBlock(QGraphicsWidget):
         painter.setPen(Qt.red)
         painter.drawRect(self.rect())
 
+class MyBlockHorizontalSpacer(QGraphicsWidget):
+    def __init__(self,parent):
+        super(MyBlockHorizontalSpacer,self).__init__(parent=parent)
+        self.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding,QSizePolicy.MinimumExpanding))
+        self.setPreferredHeight(15)
+        self.setMinimumHeight(15)
+
+#     def paint(self,painter,option,widget):
+#         painter.setPen(Qt.blue)
+#         painter.drawRect(self.rect())
+
 class MyBlockSpacer(QGraphicsWidget):
+    """ Spacer between blocks, associated with the block to its left """
     def __init__(self,parent):
         super(MyBlockSpacer,self).__init__(parent=parent)
         self.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding,QSizePolicy.Preferred))
         self.setPreferredWidth(15)
         self.setMinimumWidth(15)
 
+class MyContainer(QGraphicsWidget):
+    """ Emitter or Collector """
+    def __init__(self,parent):
+        super(MyContainer,self).__init__(parent=parent)
+        self.parent = parent
+
+    def paint(self,painter,option,widget):
+        painter.setPen(Qt.green)
+        painter.drawRect(self.rect())
 
 
 class MySnap(QGraphicsWidget):
-    def __init__(self,parent):
+    def __init__(self,parent,snap):
         super(MySnap,self).__init__(parent=parent)
+        # The parent here should be a MyContainer
         self.setSizePolicy(QSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred))
         self.setPreferredWidth(20)
         self.setPreferredHeight(40)
+        # Link with the snap
+        self.snap = snap
+        self.snap.visual = self
+
+        self.rightSpacer = MySnap.Spacer(self)
         
     def paint(self,painter,option,widget):
         painter.setPen(Qt.green)
         painter.drawRect(self.rect())
+
+
+    class Spacer(QGraphicsWidget):
+        """ Snap Spacer """
+        def __init__(self,parent):
+            super(Spacer,self).__init__(parent=parent)
+            self.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding,QSizePolicy.Preferred))
+            self.setPreferredWidth(15)
+            self.setMinimumWidth(15)
+
 
 
 class MyArc(QGraphicsWidget):
@@ -78,60 +139,9 @@ class DrawingBoard(QGraphicsWidget):
         super(DrawingBoard,self).__init__(parent=None)
         self.setAcceptedMouseButtons(Qt.LeftButton)
         self.resize(0,0)
-
-#         a = MyBlock(self)
-#         b = MyBlockSpacer(self)
-#         c = MyBlock(self)
-# 
-#         d = MyArc(self)
-#         c.setLayout(QGraphicsLinearLayout(Qt.Horizontal))
-# 
-# 
-#         e1 = MySnap(self)
-#         e2 = MySnap(self)
-#         e3 = MySnap(self)
-#         e4 = MySnap(self)
-#         e5 = MySnap(self)
-#         e6 = MySnap(self)
-# 
-# 
         l = QGraphicsAnchorLayout()
         l.setSpacing(0)
-
         self.setLayout(l)
-#         # Vertical
-#         l.addAnchor(a, Qt.AnchorTop, l, Qt.AnchorTop)
-#         l.addAnchor(a, Qt.AnchorLeft,l,Qt.AnchorLeft)
-#         l.addAnchor(a, Qt.AnchorTop,b,Qt.AnchorTop)
-#         l.addAnchor(a, Qt.AnchorRight, b, Qt.AnchorLeft)
-#         l.addAnchor(b, Qt.AnchorTop,c,Qt.AnchorTop)
-#         l.addAnchor(b, Qt.AnchorRight,c,Qt.AnchorLeft)
-# 
-#         l.addAnchor(a, Qt.AnchorLeft, e1, Qt.AnchorLeft)
-#         l.addAnchor(a, Qt.AnchorVerticalCenter, e1, Qt.AnchorVerticalCenter)
-#         l.addAnchor(e1, Qt.AnchorRight, e2, Qt.AnchorLeft)
-#         l.addAnchor(e2, Qt.AnchorRight, e3, Qt.AnchorLeft)
-#         l.addAnchor(e3, Qt.AnchorRight, e4, Qt.AnchorLeft)
-#         l.addAnchor(e4, Qt.AnchorRight, e5, Qt.AnchorLeft)
-#         l.addAnchor(e5, Qt.AnchorRight, e6, Qt.AnchorLeft)
-#         l.addAnchor(e6, Qt.AnchorRight, a, Qt.AnchorRight)
-# 
-# 
-#         f1 = MySnap(c)
-#         f2 = MySnap(c)
-#         f3 = MySnap(c)
-#         f4 = MySnap(c)
-#         f5 = MySnap(c)
-#  
-#         l.addAnchor(e6,Qt.AnchorRight,d,Qt.AnchorLeft)
-#         l.addAnchor(d,Qt.AnchorRight,f1,Qt.AnchorLeft)
-# 
-# 
-#         c.layout().addItem(f1)
-#         c.layout().addItem(f2)
-#         c.layout().addItem(f3)
-#         c.layout().addItem(f4)
-#         c.layout().addItem(f5)
 
     def autoLayout(self,topology):
 
@@ -141,8 +151,12 @@ class DrawingBoard(QGraphicsWidget):
             print "adding block",index
             vertexBlock = MyBlock(self,block)
             visualBlocks.append(vertexBlock)
+
+        # Anchor the first block against the layout
         self.layout().addAnchor(visualBlocks[0],Qt.AnchorTop,self.layout(),Qt.AnchorTop)
         self.layout().addAnchor(visualBlocks[0],Qt.AnchorLeft,self.layout(),Qt.AnchorLeft)
+
+        # Start anchoring the other blocks
         for b in visualBlocks:
             b.link()
         self.layout().invalidate()
