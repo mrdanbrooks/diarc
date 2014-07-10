d2848b7dfb413ef1112570c0e1f2879da70281b9
dan brooks <dan@danbrooks.net>
2014-07-10 16:38:48 -0700

    [db] moves ros system graph updating functionality direclty into the RSG

--- ros_topology.py
+++ ros_topology.py
@@ -11,11 +11,14 @@
 #   Source = Publisher
 #
 from topology import *
+import rosgraph
+import rosnode
+QUIET_NAMES = ['/rosout']
 
 class RosSystemGraph(Topology):
     def __init__(self):
         super(RosSystemGraph,self).__init__()
-        # Renaming these things just for convenience
+        self._master = rosgraph.Master('/RosSystemGraph')
 
     @property
     def nodes(self):
@@ -33,6 +36,78 @@ class RosSystemGraph(Topology):
         """ returns a 2-tuple of (posAltitude,negAltitude) of the avaliable altitudes """
         altitudes = [band.altitude for band in self.bands.values()] + [0]
         return (max(altitudes)+1,min(altitudes)-1)
+
+    def update(self):
+        print "*** UPDATING GRAPH ***"
+        # Query master and compile a list of all published topics and their types
+        allCurrentTopics = self._master.getPublishedTopics('/')
+        allCurrentTopicNames = [x[0] for x in allCurrentTopics]
+        # Get all the topics we currently know about
+        rsgTopics = self.topics
+
+        # Remove any topics from Ros System Graph not currently known to master
+        for topic in rsgTopics.values():
+            if topic.name not in allCurrentTopicNames:
+                print "Removing Topic",topic.name, "not found in ",allCurrentTopicNames
+                topic.release()
+
+        # Add any topics not currently in the Ros System Graph
+        for topicName, topicType in allCurrentTopics:
+            if topicName not in rsgTopics: # and topicName not in QUIET_NAMES:
+                topic = Topic(self,topicName,topicType)
+#                 topic.posBand.visual = self.visualBandType()
+
+        # Compile a list of node names
+        allCurrentNodes = rosnode.get_node_names()
+        # Get all nodes we currently know about
+        rsgNodes = self.nodes
+
+        # Remove any nodes from RosSystemGraph not currently known to master
+        for node in rsgNodes.values():
+            if node.name not in allCurrentNodes:
+                print "Removing Node",node.name, "not found in ",allCurrentNodes
+                node.release()
+
+        # Add any nodes not currently in the Ros System Graph
+        for name in allCurrentNodes:
+            if name not in rsgNodes: # and name not in QUIET_NAMES:
+                node = Node(self,name)
+                try:
+                    node.location = self._master.lookupNode(name)
+                except socket.error:
+                    raise Exception("Unable to communicate with master!")
+
+        # Check for added or removed connections
+        systemState = self._master.getSystemState()
+        # Process publishers
+        for topicName, publishersList in systemState[0]:
+            if topicName in QUIET_NAMES: 
+                continue
+            rsgPublishers = self.topics[topicName].publishers
+            # Remove publishers that don't exist anymore
+            for publisher in rsgPublishers:
+                if publisher.node.name not in publishersList:
+                    publisher.release()
+            # Add publishers taht are not yet in the RosSystemGraph
+            for nodeName in publishersList:
+                if nodeName not in [pub.node.name for pub in rsgPublishers]:
+                    publisher = Publisher(self,self.nodes[nodeName],self.topics[topicName])
+
+        # Process subscribers
+        for topicName, subscribersList in systemState[1]:
+            if topicName in QUIET_NAMES: 
+                continue
+            rsgSubscribers = self.topics[topicName].subscribers
+            # Remove subscribers that don't exist anymore
+            for subscriber in rsgSubscribers:
+                if subscriber.node.name not in subscribersList:
+                    subscriber.release()
+            # Add subscriber taht are not yet in the RosSystemGraph
+            for nodeName in subscribersList:
+                if nodeName not in [sub.node.name for sub in rsgSubscribers]:
+                    subscriber = Subscriber(self,self.nodes[nodeName],self.topics[topicName])
+        print "*** FINISHED UPDATING ***"
+
 
 
 
