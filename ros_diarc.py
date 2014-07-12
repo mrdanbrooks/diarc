74d21647079f7e25347d532f64d3585faf349026
dan brooks <dan@danbrooks.net>
2014-07-12 11:30:17 -0700

    [db] moved new object callback from GraphView into Topology widget

--- ros_diarc.py
+++ ros_diarc.py
@@ -7,7 +7,24 @@ class RosTopologyWidget(TopologyWidget):
     def __init__(self,topology,diarcWidget):
         super(RosTopologyWidget,self).__init__(topology)
         self.diarcWidget = diarcWidget
+        self.topology.new_object_callback = lambda x: self.new_object_callback(x)
 
+    def new_object_callback(self,obj):
+        print "new object is a ",type(obj)
+        if isinstance(obj,Topic):
+            print "Adding Band",obj.posBand.altitude
+            BandItem(self,obj.posBand)
+            print "Adding Band",obj.negBand.altitude
+            BandItem(self,obj.negBand)
+        elif isinstance(obj,Node):
+            print "Adding Block",obj.block.index
+            BlockItem(self,obj.block)
+        elif isinstance(obj,Publisher) or isinstance(obj,Subscriber):
+            print "adding snap",obj.snap.order
+            SnapItem(self,obj.snap)
+        else:
+            raise Exception("Unexpected type %r"%type(obj))
+ 
     def mousePressEvent(self,event):
         self.topology.update()
         self.link()
@@ -23,13 +40,12 @@ class RosTopologyWidget(TopologyWidget):
             print t,objTypes[t]
         print "total:",self.layout().count()
 
-        
+       
 class RosDiarcWidget(QGraphicsView):
     def __init__(self,context):
         super(RosDiarcWidget,self).__init__(context)
         # Instantiate a diarc topology
         self.rsg = LiveRosSystemGraph() 
-        self.rsg.new_object_callback = lambda x: self.new_object_callback(x)
 
         # Set up the scene and add the topology widget to it 
         self.setScene(QGraphicsScene(self))
@@ -43,22 +59,7 @@ class RosDiarcWidget(QGraphicsView):
         self.resize(1024,768)
         self.show()
 
-    def new_object_callback(self,obj):
-        print "new object is a ",type(obj)
-        if isinstance(obj,Topic):
-            print "Adding Band",obj.posBand.altitude
-            BandItem(self.topologyWidget,obj.posBand)
-            print "Adding Band",obj.negBand.altitude
-            BandItem(self.topologyWidget,obj.negBand)
-        elif isinstance(obj,Node):
-            print "Adding Block",obj.block.index
-            BlockItem(self.topologyWidget,obj.block)
-        elif isinstance(obj,Publisher) or isinstance(obj,Subscriber):
-            print "adding snap",obj.snap.order
-            SnapItem(self.topologyWidget,obj.snap)
-        else:
-            raise Exception("Unexpected type %r"%type(obj))
-            
+           
     def wheelEvent(self,event):
         self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
         scaleFactor = 1.15
