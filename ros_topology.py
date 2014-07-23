dc3e3ee79e2bc7db4b14318fcf19f2bba14409e1
dan brooks <dan@danbrooks.net>
2014-07-23 15:06:05 -0700

    [db] removed LiveRosSystemGraph - functionality now in ros_adapter.py

--- ros_topology.py
+++ ros_topology.py
@@ -11,9 +11,6 @@
 #   Source = Publisher
 #
 from topology import *
-import rosgraph
-import rosnode
-QUIET_NAMES = ['/rosout','/tf']
 
 class RosSystemGraph(Topology):
     def __init__(self):
@@ -35,102 +32,6 @@ class RosSystemGraph(Topology):
         """ returns a 2-tuple of (posAltitude,negAltitude) of the avaliable altitudes """
         altitudes = [band.altitude for band in self.bands.values()] + [0]
         return (max(altitudes)+1,min(altitudes)-1)
-
-
-
-class LiveRosSystemGraph(RosSystemGraph):
-    def __init__(self):
-        super(LiveRosSystemGraph,self).__init__()
-        self._master = rosgraph.Master('/RosSystemGraph')
-        # This callback will pass the object created
-        self.new_object_callback = None
-
-    def update(self):
-        print "*** UPDATING GRAPH ***"
-        # Query master and compile a list of all published topics and their types
-        allCurrentTopics = self._master.getPublishedTopics('/')
-        allCurrentTopicNames = [x[0] for x in allCurrentTopics]
-        # Get all the topics we currently know about
-        rsgTopics = self.topics
-
-        # Remove any topics from Ros System Graph not currently known to master
-        for topic in rsgTopics.values():
-            if topic.name not in allCurrentTopicNames:
-                print "Removing Topic",topic.name, "not found in ",allCurrentTopicNames
-                topic.release()
-
-        # Add any topics not currently in the Ros System Graph
-        for topicName, topicType in allCurrentTopics:
-            if topicName not in rsgTopics: # and topicName not in QUIET_NAMES:
-                topic = Topic(self,topicName,topicType)
-                if self.new_object_callback:
-                    self.new_object_callback(topic)
-#                 topic.posBand.visual = self.visualBandType()
-
-        # Compile a list of node names
-        allCurrentNodes = rosnode.get_node_names()
-        # Get all nodes we currently know about
-        rsgNodes = self.nodes
-
-        # Remove any nodes from RosSystemGraph not currently known to master
-        for node in rsgNodes.values():
-            if node.name not in allCurrentNodes:
-                print "Removing Node",node.name, "not found in ",allCurrentNodes
-                node.release()
-
-        # Add any nodes not currently in the Ros System Graph
-        for name in allCurrentNodes:
-            if name not in rsgNodes: # and name not in QUIET_NAMES:
-                node = Node(self,name)
-                if self.new_object_callback:
-                    self.new_object_callback(node)
-                try:
-                    node.location = self._master.lookupNode(name)
-                except socket.error:
-                    raise Exception("Unable to communicate with master!")
-
-        # Check for added or removed connections
-        systemState = self._master.getSystemState()
-        # Process publishers
-        for topicName, publishersList in systemState[0]:
-            if topicName in QUIET_NAMES: 
-                continue
-            rsgPublishers = self.topics[topicName].publishers
-            # Remove publishers that don't exist anymore
-            for publisher in rsgPublishers:
-                if publisher.node.name not in publishersList:
-                    publisher.release()
-            # Add publishers taht are not yet in the RosSystemGraph
-            for nodeName in publishersList:
-                if nodeName not in [pub.node.name for pub in rsgPublishers]:
-                    publisher = Publisher(self,self.nodes[nodeName],self.topics[topicName])
-                    if self.new_object_callback:
-                        self.new_object_callback(publisher)
-
-        # Process subscribers
-        for topicName, subscribersList in systemState[1]:
-            if topicName in QUIET_NAMES: 
-                continue
-            try:
-                rsgSubscribers = self.topics[topicName].subscribers
-            except:
-                print topicName,"not found in"
-                continue
-            # Remove subscribers that don't exist anymore
-            for subscriber in rsgSubscribers:
-                if subscriber.node.name not in subscribersList:
-                    subscriber.release()
-            # Add subscriber taht are not yet in the RosSystemGraph
-            for nodeName in subscribersList:
-                if nodeName not in [sub.node.name for sub in rsgSubscribers]:
-                    subscriber = Subscriber(self,self.nodes[nodeName],self.topics[topicName])
-                    if self.new_object_callback:
-                        self.new_object_callback(subscriber)
-
-        print "*** FINISHED UPDATING ***"
-
-
-
 
 
 
@@ -232,10 +133,4 @@ class Subscriber(Sink):
         # NOTE: See note on Node class about why this MUST be a property.
         return self.vertex
 
-
-
-if __name__ == "__main__":
-    ros = RosSystemGraph()
-    joystick = Node(ros)
-    velocity = Topic(ros)
 
